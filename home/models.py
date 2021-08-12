5fec925cc311243120d4666bb1eeb2f249a0e30f
Tufayel Ahmed <mailtochoyon@gmail.com>
2021-08-12 17:55:20 +0600

    increased post content size

--- home/models.py
+++ home/models.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class BlogPost(models.Model):
     blog_slug = models.CharField(max_length=100, default='')
     blog_title = models.CharField(max_length=100)
-    blog_text = models.TextField(max_length=10000)
+    blog_text = models.TextField(max_length=1024280)
     blog_owner = models.CharField(max_length=15)
     post_time = models.DateField(default=django.utils.timezone.now)
 
